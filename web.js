9bd0684d6691f67d1b4af34d69f3bd3e5f6108d8
Arthur Darcet <arthur.darcet@m4x.org>
2013-07-04 11:01:31 +0200

    read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,4 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var data = require('fs').readFileSync('index.html').toString();
+require('express').createServer().listen(process.env.PORT || 5000).get('/', function(res, req) {
+	req.send(data);
 });
